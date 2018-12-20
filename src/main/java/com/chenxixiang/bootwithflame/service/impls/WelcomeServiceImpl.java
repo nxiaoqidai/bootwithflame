fd74de99e32d8acfb44008413b38ed56cf0d6bf9
Xixiang Chen <nxiaoqidai@qq.com>
2018-12-20 09:46:49 +0800

    try githubdesk in company

--- src/main/java/com/chenxixiang/bootwithflame/service/impls/WelcomeServiceImpl.java
+++ src/main/java/com/chenxixiang/bootwithflame/service/impls/WelcomeServiceImpl.java
@@ -1,15 +1,11 @@
 package com.chenxixiang.bootwithflame.service.impls;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.chenxixiang.bootwithflame.dao.impl.UserDao;
 import com.chenxixiang.bootwithflame.service.interfaces.WelcomeService;
 
 @Service
 public class WelcomeServiceImpl implements WelcomeService {
-	@Autowired
-	UserDao dao;
 
 	public String foo() {
 		return "FOO method ^-^";
