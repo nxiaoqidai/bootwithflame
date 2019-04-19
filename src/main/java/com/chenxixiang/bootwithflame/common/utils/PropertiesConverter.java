563cb7fee40db34e3860df3f3687519fe2233ce2
xixiang_chen <xixiang_chen@kingdee.com>
2019-04-19 15:43:22 +0800

    新增pojo对象合并方法

--- src/main/java/com/chenxixiang/bootwithflame/common/utils/PropertiesConverter.java
+++ src/main/java/com/chenxixiang/bootwithflame/common/utils/PropertiesConverter.java
@@ -1,18 +1,9 @@
 package com.chenxixiang.bootwithflame.common.utils;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeansException;
 
-import com.chenxixiang.bootwithflame.service.impls.UserServiceImpl;
-
-import ch.qos.logback.classic.Logger;
-import ch.qos.logback.classic.LoggerContext;
-
 public class PropertiesConverter {
-
-	private static Logger LOGGER = ((LoggerContext) LoggerFactory.getILoggerFactory())
-			.getLogger(UserServiceImpl.class);
 
 	public static <T, O> T convertObj(O originObj, Class<T> clazz) {
 		if (originObj == null || clazz == null) {
@@ -23,11 +14,18 @@ public class PropertiesConverter {
 			targetObj = clazz.newInstance();
 			BeanUtils.copyProperties(originObj, targetObj);
 		} catch (BeansException | InstantiationException | IllegalAccessException e) {
-			LOGGER.error(e.getMessage());
 			e.printStackTrace();
 			return null;
 		}
 		return targetObj;
+	}
+
+	public static <T, O> T mapProperties(O mapFromObject, T mapToObject) {
+		if (mapFromObject == null || mapToObject == null) {
+			throw new IllegalArgumentException("Object convert param must not be null.");
+		}
+		BeanUtils.copyProperties(mapFromObject, mapToObject);
+		return mapToObject;
 	}
 
 }
