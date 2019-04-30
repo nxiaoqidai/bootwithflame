7f2dd5f6aaa20a14ec1969d89bde939e405972ea
xixiang_chen <xixiang_chen@kingdee.com>
2019-04-30 09:43:53 +0800

    添加stomp-websocket框架

--- src/main/java/com/chenxixiang/bootwithflame/common/configs/Swagger2Config.java
+++ src/main/java/com/chenxixiang/bootwithflame/common/configs/Swagger2Config.java
@@ -1,4 +1,4 @@
-package com.chenxixiang.bootwithflame.web;
+package com.chenxixiang.bootwithflame.common.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
